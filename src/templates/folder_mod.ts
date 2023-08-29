babf8daf2a201c6c93cd8c0feddb1b5e8ee01307
AFCMS <afcm.contact@gmail.com>
2023-08-30 00:19:50 +0200

    Improve game mod template

--- src/templates/folder_mod.ts
+++ src/templates/folder_mod.ts
@@ -1,7 +1,9 @@
 import * as vscode from "vscode";
+import * as fs from "fs";
 import * as path from "path";
 
 import utils from "../utils";
+import config from "../config";
 
 function setupFolderModFiles(path: string, name: string) {
     utils.makeFiles(
@@ -36,6 +38,31 @@ $0`);
 function setupModInFolder(u: vscode.Uri) {
     let folder = u.fsPath;
 
+    const w = vscode.workspace.getWorkspaceFolder(u);
+
+    if (!w) {
+        vscode.window.showErrorMessage("Given URI isn't in a workspace folder!");
+        return;
+    }
+
+    const context = config.getContext(w);
+
+    // TODO: add modpack context
+    if (context === undefined || context !== "game") {
+        vscode.window.showErrorMessage("Workspace isn't a game or a modpack!");
+        return;
+    }
+
+    // const wPath = w.uri.fsPath;
+
+    // TODO: handle modpacks
+    if (context === "game") {
+        if (path.basename(folder) !== "mods") {
+            vscode.window.showErrorMessage("Folder can't handle mods!");
+            return;
+        }
+    }
+
     vscode.window
         .showInputBox({
             title: "Create Mod",
@@ -46,9 +73,18 @@ function setupModInFolder(u: vscode.Uri) {
                         message: "Mod name can't be empty",
                         severity: vscode.InputBoxValidationSeverity.Error,
                     };
-                } else if (!utils.validateModName(value)) {
+                }
+
+                if (!utils.validateModName(value)) {
                     return {
                         message: "Invalid mod name, can only contain [a-z_]",
+                        severity: vscode.InputBoxValidationSeverity.Error,
+                    };
+                }
+
+                if (fs.existsSync(path.join(folder, value))) {
+                    return {
+                        message: "A folder with that name already exist",
                         severity: vscode.InputBoxValidationSeverity.Error,
                     };
                 }
@@ -58,8 +94,11 @@ function setupModInFolder(u: vscode.Uri) {
         })
         .then((value) => {
             if (value) {
+                // Create mod folder
+                fs.mkdirSync(path.join(folder, value));
+
                 // Setup mod folder
-                setupFolderModFiles(folder, value);
+                setupFolderModFiles(path.join(folder, value), value);
 
                 // Open main mod.conf file
                 utils.openWithSnippet(path.join(folder, value, "mod.conf"), folderModCompletionSnippet(value));
