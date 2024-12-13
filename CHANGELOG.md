61740ed2c10fe3a73a36c7451be3c1822e603507
Mark Wiemer <markwiemer@outlook.com>
2024-12-13 09:33:37 -0600

    Format using 4 spaces

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,53 +1,53 @@
 ### 1.4.3: November 4, 2024
 
-- API autocompletion bumped to Minetest version 5.9.1
+-   API autocompletion bumped to Minetest version 5.9.1
 
 ### 1.4.2: May 2, 2023
 
-- API autocompletion bumped to Minetest version 5.7.0
-- Only store API reference line numbers in snippet file to reduce size
+-   API autocompletion bumped to Minetest version 5.7.0
+-   Only store API reference line numbers in snippet file to reduce size
 
 ### 1.4.1: June 12, 2021
 
-- Fixed function arguments breaking on certain characters
-- Fixed autocompletion of constants
+-   Fixed function arguments breaking on certain characters
+-   Fixed autocompletion of constants
 
 ### 1.4.0: April 30, 2021
 
-- API autocompletion bumped to Minetest version 5.4.1
-- Rewrote snippet generator
-  - No longer requires manual input
-  - Captures all API methods, tables, and constants
-  - Adjusted quick info formatting
-  - Includes API links
-- CompletionItemProvider can handle some backspacing now
-- Texture modifiers will remove trailing bracket if autocompleted
-- Global constructors and namespaces now have snippets
-- Added missing globals to .luacheckrc
-- Fixed and tweaked formspec highlighting
+-   API autocompletion bumped to Minetest version 5.4.1
+-   Rewrote snippet generator
+    -   No longer requires manual input
+    -   Captures all API methods, tables, and constants
+    -   Adjusted quick info formatting
+    -   Includes API links
+-   CompletionItemProvider can handle some backspacing now
+-   Texture modifiers will remove trailing bracket if autocompleted
+-   Global constructors and namespaces now have snippets
+-   Added missing globals to .luacheckrc
+-   Fixed and tweaked formspec highlighting
 
 ### 1.3.1: January 18, 2020
 
-- Fixed incorrect method snippets
+-   Fixed incorrect method snippets
 
 ### 1.3.0: January 13, 2020
 
-- Reworked intellisense
+-   Reworked intellisense
 
 ### 1.2.0: January 13, 2020
 
-- Improved autocomplete suggestions
-- Fixed snippets not working with other Intellisense extensions
+-   Improved autocomplete suggestions
+-   Fixed snippets not working with other Intellisense extensions
 
 ### 1.1.0: January 10, 2020
 
-- Added formspec string syntax highlighting
+-   Added formspec string syntax highlighting
 
 ### 1.0.1: January 10, 2020
 
-- Fixed extension not loading when not in a workspace
-- Fixed workspace-only commands running when not in a workspace
+-   Fixed extension not loading when not in a workspace
+-   Fixed workspace-only commands running when not in a workspace
 
 ### 1.0.0: January 10, 2020
 
-- Initial release
+-   Initial release
--- extension.js
+++ extension.js
@@ -4,11 +4,11 @@ const path = require("path");
 const snippets = require("./smartsnippets.json");
 
 const rootPath =
-  vscode.workspace.workspaceFolders != undefined
-    ? vscode.workspace.workspaceFolders[0].uri.fsPath
-    : "";
+    vscode.workspace.workspaceFolders != undefined
+        ? vscode.workspace.workspaceFolders[0].uri.fsPath
+        : "";
 const doc_api_link =
-  "\n\n[View in lua_api.md](https://github.com/minetest/minetest/blob/5.9.1/doc/lua_api.md?plain=1#";
+    "\n\n[View in lua_api.md](https://github.com/minetest/minetest/blob/5.9.1/doc/lua_api.md?plain=1#";
 const luacheckrc = `read_globals = {
     "DIR_DELIM", "INIT",
 
@@ -56,193 +56,202 @@ const luacheckrc = `read_globals = {
 }`;
 
 function makeFiles(files, folders) {
-  for (const folder of folders) {
-    const fullpath = path.join(rootPath, folder);
-    if (!fs.existsSync(fullpath)) {
-      fs.mkdirSync(fullpath);
+    for (const folder of folders) {
+        const fullpath = path.join(rootPath, folder);
+        if (!fs.existsSync(fullpath)) {
+            fs.mkdirSync(fullpath);
+        }
     }
-  }
-  for (const file of files) {
-    const fullpath = path.join(rootPath, file.name);
-    if (!fs.existsSync(fullpath)) {
-      fs.writeFileSync(fullpath, file.content);
+    for (const file of files) {
+        const fullpath = path.join(rootPath, file.name);
+        if (!fs.existsSync(fullpath)) {
+            fs.writeFileSync(fullpath, file.content);
+        }
     }
-  }
 }
 
 function activate(context) {
-  // Intellisense
-  let completion = vscode.languages.registerCompletionItemProvider(
-    { language: "lua", scheme: "file" },
-    {
-      provideCompletionItems(document, position) {
-        // Only show snippets if in a Minetest workspace
-        if (
-          vscode.workspace
-            .getConfiguration("minetest-tools")
-            .get("workspaceOnly") &&
-          !(
-            fs.existsSync(path.join(rootPath, "init.lua")) ||
-            fs.existsSync(path.join(rootPath, "mods")) ||
-            fs.existsSync(path.join(rootPath, "modpack.txt"))
-          )
-        )
-          return [];
+    // Intellisense
+    let completion = vscode.languages.registerCompletionItemProvider(
+        { language: "lua", scheme: "file" },
+        {
+            provideCompletionItems(document, position) {
+                // Only show snippets if in a Minetest workspace
+                if (
+                    vscode.workspace
+                        .getConfiguration("minetest-tools")
+                        .get("workspaceOnly") &&
+                    !(
+                        fs.existsSync(path.join(rootPath, "init.lua")) ||
+                        fs.existsSync(path.join(rootPath, "mods")) ||
+                        fs.existsSync(path.join(rootPath, "modpack.txt"))
+                    )
+                )
+                    return [];
 
-        const line = document.getText(
-          new vscode.Range(new vscode.Position(position.line, 0), position),
-        );
-        const afterpos = new vscode.Range(
-          position,
-          new vscode.Position(position.line, position.character + 1),
-        );
-        const after = document.getText(afterpos);
+                const line = document.getText(
+                    new vscode.Range(
+                        new vscode.Position(position.line, 0),
+                        position,
+                    ),
+                );
+                const afterpos = new vscode.Range(
+                    position,
+                    new vscode.Position(position.line, position.character + 1),
+                );
+                const after = document.getText(afterpos);
 
-        let items = [];
+                let items = [];
 
-        for (const snippet of snippets) {
-          if (
-            snippet.token &&
-            line.match(
-              new RegExp(
-                `${snippet.token.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&")}(?![^\\w\\n\\s\\r\\-])[\\w\\n\\s\\r\\-]*`,
-              ),
-            )
-          ) {
-            let item = new vscode.CompletionItem(snippet.prefix);
-            item.insertText = new vscode.SnippetString(snippet.body);
-            item.documentation = new vscode.MarkdownString(
-              snippet.desc +
-                (snippet.doc_lines
-                  ? doc_api_link + snippet.doc_lines + ")"
-                  : ""),
-            );
-            item.kind = snippet.kind;
-            item.detail = snippet.detail || snippet.prefix;
-            item.additionalTextEdits =
-              snippet.token.match(/^[\(\[\{]$/) && after.match(/[\}\]\)]/)
-                ? [vscode.TextEdit.delete(afterpos)]
-                : null;
+                for (const snippet of snippets) {
+                    if (
+                        snippet.token &&
+                        line.match(
+                            new RegExp(
+                                `${snippet.token.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&")}(?![^\\w\\n\\s\\r\\-])[\\w\\n\\s\\r\\-]*`,
+                            ),
+                        )
+                    ) {
+                        let item = new vscode.CompletionItem(snippet.prefix);
+                        item.insertText = new vscode.SnippetString(
+                            snippet.body,
+                        );
+                        item.documentation = new vscode.MarkdownString(
+                            snippet.desc +
+                                (snippet.doc_lines
+                                    ? doc_api_link + snippet.doc_lines + ")"
+                                    : ""),
+                        );
+                        item.kind = snippet.kind;
+                        item.detail = snippet.detail || snippet.prefix;
+                        item.additionalTextEdits =
+                            snippet.token.match(/^[\(\[\{]$/) &&
+                            after.match(/[\}\]\)]/)
+                                ? [vscode.TextEdit.delete(afterpos)]
+                                : null;
 
-            items.push(item);
-          }
-        }
+                        items.push(item);
+                    }
+                }
 
-        return items;
-      },
-    },
-    ":",
-    ".",
-    "[",
-  );
-
-  // Mod boilerplate
-  let modproject = vscode.commands.registerCommand(
-    "extension.modProject",
-    () => {
-      if (rootPath == "") return;
-      const name = vscode.workspace.name;
-      const files = [
-        {
-          name: "init.lua",
-          content: "",
+                return items;
+            },
         },
-        {
-          name: "mod.conf",
-          content: `name = ${name}\ndescription = \ndepends = \noptional_depends = `,
-        },
-        {
-          name: "README.md",
-          content: "",
-        },
-        {
-          name: "LICENSE.txt",
-          content: "",
-        },
-        {
-          name: ".luacheckrc",
-          content: luacheckrc,
-        },
-      ];
-      const folders = ["textures", "models", "sounds"];
-      makeFiles(files, folders);
-    },
-  );
-
-  // Game boilerplate
-  let gameproject = vscode.commands.registerCommand(
-    "extension.gameProject",
-    () => {
-      if (rootPath == "") return;
-      const name = vscode.workspace.name;
-      const files = [
-        {
-          name: "game.conf",
-          content: `name = ${name
-            .replace(/[_-]/g, " ")
-            .replace(/(^| )(\w)/g, function (m) {
-              return m.toUpperCase();
-            })}\nauthor = \ndescription = `,
-        },
-        {
-          name: "README.md",
-          content: "",
-        },
-        {
-          name: "LICENSE.txt",
-          content: "",
-        },
-        {
-          name: ".luacheckrc",
-          content: luacheckrc,
-        },
-      ];
-      const folders = ["menu", "mods"];
-      makeFiles(files, folders);
-    },
-  );
-
-  // .luacheckrc generator
-  let luacheck = vscode.commands.registerCommand("extension.luacheckrc", () => {
-    if (rootPath == "") return;
-    makeFiles(
-      [
-        {
-          name: ".luacheckrc",
-          content: luacheckrc,
-        },
-      ],
-      [],
+        ":",
+        ".",
+        "[",
     );
-  });
 
-  // Toggle workspace-only snippets
-  let toggle = vscode.commands.registerCommand(
-    "extension.workspaceToggle",
-    () => {
-      const conf = vscode.workspace.getConfiguration("minetest-tools");
-      const newVal = conf.get("workspaceOnly") ? false : true;
-      conf.update("workspaceOnly", newVal, true);
-      vscode.window.showInformationMessage(
-        newVal
-          ? "Minetest Intellisense active in workspace only."
-          : "Minetest Intellisense active for all Lua files.",
-      );
-    },
-  );
+    // Mod boilerplate
+    let modproject = vscode.commands.registerCommand(
+        "extension.modProject",
+        () => {
+            if (rootPath == "") return;
+            const name = vscode.workspace.name;
+            const files = [
+                {
+                    name: "init.lua",
+                    content: "",
+                },
+                {
+                    name: "mod.conf",
+                    content: `name = ${name}\ndescription = \ndepends = \noptional_depends = `,
+                },
+                {
+                    name: "README.md",
+                    content: "",
+                },
+                {
+                    name: "LICENSE.txt",
+                    content: "",
+                },
+                {
+                    name: ".luacheckrc",
+                    content: luacheckrc,
+                },
+            ];
+            const folders = ["textures", "models", "sounds"];
+            makeFiles(files, folders);
+        },
+    );
 
-  context.subscriptions.push(
-    completion,
-    modproject,
-    gameproject,
-    luacheck,
-    toggle,
-  );
+    // Game boilerplate
+    let gameproject = vscode.commands.registerCommand(
+        "extension.gameProject",
+        () => {
+            if (rootPath == "") return;
+            const name = vscode.workspace.name;
+            const files = [
+                {
+                    name: "game.conf",
+                    content: `name = ${name
+                        .replace(/[_-]/g, " ")
+                        .replace(/(^| )(\w)/g, function (m) {
+                            return m.toUpperCase();
+                        })}\nauthor = \ndescription = `,
+                },
+                {
+                    name: "README.md",
+                    content: "",
+                },
+                {
+                    name: "LICENSE.txt",
+                    content: "",
+                },
+                {
+                    name: ".luacheckrc",
+                    content: luacheckrc,
+                },
+            ];
+            const folders = ["menu", "mods"];
+            makeFiles(files, folders);
+        },
+    );
 
-  console.log("Minetest Tools extension is active.");
+    // .luacheckrc generator
+    let luacheck = vscode.commands.registerCommand(
+        "extension.luacheckrc",
+        () => {
+            if (rootPath == "") return;
+            makeFiles(
+                [
+                    {
+                        name: ".luacheckrc",
+                        content: luacheckrc,
+                    },
+                ],
+                [],
+            );
+        },
+    );
+
+    // Toggle workspace-only snippets
+    let toggle = vscode.commands.registerCommand(
+        "extension.workspaceToggle",
+        () => {
+            const conf = vscode.workspace.getConfiguration("minetest-tools");
+            const newVal = conf.get("workspaceOnly") ? false : true;
+            conf.update("workspaceOnly", newVal, true);
+            vscode.window.showInformationMessage(
+                newVal
+                    ? "Minetest Intellisense active in workspace only."
+                    : "Minetest Intellisense active for all Lua files.",
+            );
+        },
+    );
+
+    context.subscriptions.push(
+        completion,
+        modproject,
+        gameproject,
+        luacheck,
+        toggle,
+    );
+
+    console.log("Minetest Tools extension is active.");
 }
 exports.activate = activate;
 
 module.exports = {
-  activate,
+    activate,
 };
